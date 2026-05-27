f61955bd80c351f01cc57eb4859203aa751d7b54
Zak Kolar <zakkolar@users.noreply.github.com>
2026-05-27 18:37:30 -0400

    create default favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  }
 })
